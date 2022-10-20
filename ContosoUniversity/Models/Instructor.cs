f091610ae6a34124640fec21cb3936ed5903ed8c
amaasik <75305698+amaasik@users.noreply.github.com>
2022-10-20 15:52:30 +0300

    Add project files.

---
+++ ContosoUniversity/Models/Instructor.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.VisualBasic;
+using System;
+
+namespace ContosoUniversity.Models
+{
+    public class Instructor
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Column("FirstName")]
+        [Display(Name = "First Name")]
+        public string FirstMidName { get; set; }
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return LastName + ", " + FirstMidName;
+            }
+
+        }
+        public DateTime HireDate { get; set; }
+        public ICollection<CourseAssignment> CourseAssignments { get; set; }
+        public OfficeAssignment OfficeAssignment { get; set; }
+    }
+
+}
